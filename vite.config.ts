3abb34a54b4a4a8ac6121763d74957dfca44eb01
cw222kq <lotten_w@hotmail.com>
2024-11-16 15:30:58 +0100

    Modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/presentation"
+  base: "/presentation",
+  build: {
+    outDir: 'dist', 
+  }
 });
