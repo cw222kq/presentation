259609b046a6b64a38ef1243462e6012607ad6a9
cw222kq <lotten_w@hotmail.com>
2024-11-21 12:13:11 +0100

    Add functionality to highlight the current page in the menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,48 @@
 import React, { useState, useEffect, useRef } from "react"
 import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5"
+import { useLocation } from "react-router-dom"
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
   const smallDeviceMenuRef = useRef<HTMLDivElement>(null)
+  const location = useLocation()
 
   const closeSmallDeviceMenuHandler = (event: MouseEvent) => {
-    if (smallDeviceMenuRef.current && !smallDeviceMenuRef.current.contains(event.target as Node)) {
+    if (
+      smallDeviceMenuRef.current &&
+      !smallDeviceMenuRef.current.contains(event.target as Node)
+    ) {
       setIsMenuOpen(false)
     }
   }
 
   useEffect(() => {
-    
     document.addEventListener("mousedown", closeSmallDeviceMenuHandler)
     return () => {
       document.removeEventListener("mousedown", closeSmallDeviceMenuHandler)
     }
   }, [])
-  
-  useEffect(() => {
-    console.log(isMenuOpen ? "opened" : "closed")
-  }, [isMenuOpen])
 
   const menu = [
     {
       id: 1,
       name: "HOME",
+      path: "/presentation/",
     },
     {
       id: 2,
       name: "SKILLS",
+      path: "/presentation/skills/",
     },
     {
       id: 3,
       name: "PROJECTS",
+      path: "/presentation/projects/",
     },
     {
       id: 4,
       name: "CONTACT",
+      path: "/presentation/contact/",
     },
   ]
   return (
@@ -67,33 +71,46 @@ const Header: React.FC = () => {
       </div>
 
       <div className="w-14 h-14 border-b bg-black justify-center items-center sm:w-24 sm:h-24 hidden md:flex">
-        <img src="/presentation/react.svg" alt="React logo" className="w-16 h-16 animate-pulse" />
+        <img
+          src="/presentation/react.svg"
+          alt="React logo"
+          className="w-16 h-16 animate-pulse"
+        />
       </div>
 
+      {/* Small device menu */}
       {isMenuOpen && (
-        <div ref={smallDeviceMenuRef} className="absolute top-12 left-0 w-48 bg-white dark:bg-black dark:text-white shadow-lg rounded-md p-4 md:hidden">
+        <div
+          ref={smallDeviceMenuRef}
+          className="absolute top-12 left-0 w-48 bg-white dark:bg-black dark:text-white shadow-lg rounded-md p-4 md:hidden"
+        >
           <ul>
             {menu.map((item) => (
               <li
                 key={item.id}
-                className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-500 cursor-pointer hover:scale-110 transition-all ease-in-out"
+                className={`px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-500 ${location.pathname === item.path ? "underline text-gray-400" : "cursor-pointer hover:scale-110 transition-all ease-in-out"}`}
               >
                 {item.name}
               </li>
             ))}
             <div className="border-t mt-2 pt-4 flex justify-center items-center gap-x-8">
-            <li><IoLogoGithub className="cursor-pointer hover:scale-110 transition-all ease-in-out" /></li>
-            <li><IoLogoLinkedin className="cursor-pointer hover:scale-110 transition-all ease-in-out" /></li>
+              <li>
+                <IoLogoGithub className="cursor-pointer hover:scale-110 transition-all ease-in-out" />
+              </li>
+              <li>
+                <IoLogoLinkedin className="cursor-pointer hover:scale-110 transition-all ease-in-out" />
+              </li>
             </div>
           </ul>
         </div>
       )}
 
+      {/* Big device menu */}
       <div className="hidden md:flex gap-x-16 lg:gap-x-24">
         {menu.map((item) => (
           <div
             key={item.id}
-            className="cursor-pointer font-medium hover:scale-110 transition-all ease-in-out"
+            className={`font-medium ${location.pathname === item.path ? "underline text-gray-400" : "cursor-pointer hover:scale-110 transition-all ease-in-out"}`}
           >
             <h2>{item.name}</h2>
           </div>
