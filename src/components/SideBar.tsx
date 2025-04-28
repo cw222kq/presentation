4cfbf3487ec2ce0f4fe01d158f193c7061ca904d
cw222kq <lotten_w@hotmail.com>
2025-04-28 20:06:30 +0200

    Fix: Vertical position of SideBar icons

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,8 +5,8 @@ const icons: Icon[] = [GithubIcon, LinkedinIcon]
 
 const SideBar: React.FC = () => {
   return (
-    <div className="w-14 hidden border-r h-screen fixed flex-col justify-around mt-14 items-center md:flex md:w-24 md:mt-24">
-      <div className="flex flex-col gap-7 text-xl pt-72 mt-48">
+    <div className="w-14 hidden border-r fixed top-14 bottom-12 flex-col justify-end items-center md:flex md:w-24 md:top-24">
+      <div className="text-xl">
         <ul className="flex flex-col gap-7">
           {icons.map((icon) => {
             const IconComponent = icon.component
@@ -17,7 +17,7 @@ const SideBar: React.FC = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label={icon.label}
-                  className="cursor-pointer hover:scale-110 transition-all ease-in-out"
+                  className="cursor-pointer hover:scale-110 transition-all ease-in-out block"
                 >
                   <IconComponent />
                 </a>
